1cc3372037c1d8264226d47e30ea161f83fe0364
Robledo Junior <robledorjunior1@gmail.com>
2023-05-04 20:07:34 -0300

    evento change

--- src/js/form.js
+++ src/js/form.js
@@ -1,13 +1,25 @@
 const inputETextArea = document.querySelectorAll('input, textarea')
 const button = document.querySelector('button')
 const form = document.querySelector('form')
+const paragrafo = document.getElementsByClassName('paragrafo-disappear')
 
 inputETextArea.forEach(input => {
   input.addEventListener('change', e => {
-    if (input.value) input.style.border = '2px solid #09d309'
-    else input.style.border = '1px solid black'
+    console.log(input.value)
+
+    const errorMessage = document.querySelector(`div#${input.name} p`)
+
+    if (input.value) {
+      input.style.border = '2px solid #09d309'
+      errorMessage.style.display = 'none'
+    }
+    else {
+      input.style.border = '1px solid black'
+      errorMessage.style.display = 'block'
+    }
   })
 })
+
 
 form.onsubmit = e => {
   e.preventDefault()
@@ -23,7 +35,7 @@ form.onsubmit = e => {
       errorMessage.style.display = 'none'
     }
     else {
-      field.style.border = '1px solid black'
+      field.style.border = '1px solid #e02323e5'
 
       errorMessage.style.display = 'block'
     }
