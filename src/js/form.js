1dfb10db6473a91dd4c32ad3ab79f03a24663a59
Robledo Junior <robledorjunior1@gmail.com>
2023-05-03 23:02:48 -0300

    primeiro commit

--- src/js/form.js
+++ src/js/form.js
@@ -1,11 +1,31 @@
-const inputETextArea = document.querySelectorAll()
+const inputETextArea = document.querySelectorAll('input, textarea')
+const button = document.querySelector('button')
+const form = document.querySelector('form')
 
 inputETextArea.forEach(input => {
-    input.addEventListener('change', () => {
-        if (input.value !== "") {
-            input.classList.add("campo-preenchido")
-        }else {
-            input.classList.remove("campo-preenchido")
-        }
-    })
+  input.addEventListener('change', e => {
+    if (input.value) input.style.border = '2px solid #09d309'
+    else input.style.border = '1px solid black'
+  })
 })
+
+form.onsubmit = e => {
+  e.preventDefault()
+
+  const fields = document.querySelectorAll('input, textarea')
+
+  fields.forEach(field => {
+    const errorMessage = document.querySelector(`div#${field.name} p`)
+
+    if (field.value) {
+      field.style.border = '2px solid #09d309'
+
+      errorMessage.style.display = 'none'
+    }
+    else {
+      field.style.border = '1px solid black'
+
+      errorMessage.style.display = 'block'
+    }
+  })
+}
